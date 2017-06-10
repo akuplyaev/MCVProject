d1ec4544409422ca5b82765d9e9a1460b9460efc
Aleksey Kuplyaev <a.kuplyaev@gmail.com>
2017-06-10 10:51:59 +0300

    Create class Employee

---
+++ MVCProject/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCProject.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get;set;}
+        public string FirstMidName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+        public string DepartmentId { get; set; }
+
+     }
+}
